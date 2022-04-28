5e2b2a03d687aa8967c23055dc855f77ee001b49
Alan Cota <acota@mulesoft.com>
2022-04-28 10:18:32 -0500

    Added build, start and postinstall scripts to package.json

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,8 @@ import routes from "./routes";
 import logger from "./logger";
 import cors from 'cors';
 
-const port = config.get("port") as number;
-const host = config.get("host") as string;
+const port = config.get("port") as number || 8080;
+const host = config.get("host") as string || "localhost";
 
 const app = express();
 
@@ -15,9 +15,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // CORS
-app.use(cors({
-    origin: 'http://localhost:3000'
-}))
+// app.use(cors({
+//     origin: 'http://localhost:3000'
+// }))
 
 app.listen(port, host, () => {
     logger.info(`Server listing at http://${host}:${port}`);
